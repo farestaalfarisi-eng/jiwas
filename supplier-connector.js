987adfdf7adc00b4e038e98ca8a35fc4fbf3981d
farestaalfarisi-eng <farestaalfarisi@gmail.com>
2026-09-21 05:59:42 +0700

    Update supplier-connector.js

--- supplier-connector.js
+++ supplier-connector.js
@@ -5,7 +5,7 @@
 
 const SupplierConnector = {
   // Ganti dengan nomor WhatsApp aktif admin (format: 628xxx tanpa tanda + atau spasi)
-  ADMIN_WA: "6281234567890",
+  ADMIN_WA: "6282255267793",
 
   // Nama brand toko untuk header format pesan
   STORE_NAME: "JIWAS ATELIER DIGITAL STORE",
